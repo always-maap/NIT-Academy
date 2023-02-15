ecaf574460f6321b8c4f5c3a9431841a888e2378
tur1ng <mohammadali.ap.2000@gmail.com>
2023-02-15 16:52:46 +0330

    fix: probability link

--- db/courses/index.ts
+++ db/courses/index.ts
@@ -1,11 +1,15 @@
 import { differentialEquations } from "./differential-equations";
 import { os } from "./os";
 import { osLab } from "./os-lab";
+import { probability } from "./probability";
 import { signalsAndSystems } from "./signals-and-systems";
 
 export const courses = {
-  "signals-and-systems": signalsAndSystems,
   "differential-equations": differentialEquations,
+  probability: probability,
+
   os: os,
   "os-lab": osLab,
+
+  "signals-and-systems": signalsAndSystems,
 };

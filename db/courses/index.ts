33dccb1b9e15e6fa8590ad61a57843ae8ce1b137
tur1ng <mohammadali.ap.2000@gmail.com>
2023-02-16 05:28:58 +0330

    feat: add persian literature

--- db/courses/index.ts
+++ db/courses/index.ts
@@ -2,6 +2,7 @@ import { Course } from "@/types/Course";
 import { differentialEquations } from "./differential-equations";
 import { os } from "./os";
 import { osLab } from "./os-lab";
+import { persianLiterature } from "./persian-literature";
 import { probability } from "./probability";
 import { signalsAndSystems } from "./signals-and-systems";
 
@@ -13,4 +14,6 @@ export const courses: Record<string, Course> = {
   "os-lab": osLab,
 
   "signals-and-systems": signalsAndSystems,
+
+  "persian-literature": persianLiterature,
 };
---
+++ db/courses/persian-literature.ts
@@ -0,0 +1,16 @@
+import { Course } from "@/types/Course";
+
+export const persianLiterature: Course = {
+  title: {
+    fa: "فارسی",
+    en: "persian-literature",
+  },
+  sources: [
+    {
+      id: "1",
+      title: "کتاب در دری",
+      type: "note",
+      url: "https://nitacademy.s3.ir-thr-at1.arvanstorage.ir/persian-literature/persian-literature.pdf",
+    },
+  ],
+};

1743cd3387e4dae0038b3e4b6f869a4e649c42bb
tur1ng <mohammadali.ap.2000@gmail.com>
2023-02-18 20:11:22 +0330

    fix: remove scroll area dark

--- components/ui/ScrollArea.tsx
+++ components/ui/ScrollArea.tsx
@@ -43,7 +43,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full dark:bg-slate-700" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-slate-700" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ));
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;

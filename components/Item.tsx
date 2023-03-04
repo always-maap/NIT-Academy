af1414805e14cf86c8545f2746465380b4d2fe2e
tur1ng <mohammadali.ap.2000@gmail.com>
2023-03-04 19:35:47 +0330

    chore: update green color

--- components/Item.tsx
+++ components/Item.tsx
@@ -9,15 +9,15 @@ type Props = {
 
 export default function Item(props: Props) {
   const colorVariants = {
-    green: "bg-[#34ed43]",
+    green: "bg-[#336347]",
     blue: "bg-[#51adef]",
     red: "bg-[#ef5151]",
     orange: "bg-[#ffb343]",
   };
 
   const shadowVariants = {
-    green: "shadow-[8px_0_0_0_#20ad2b]",
-    blue: "shadow-[8px_0_0_0_#257dbc]",
+    green: "shadow-[8px_0_0_0_#97c1a9]",
+    blue: "shadow-[8px_0_0_0_#2c5776]",
     red: "shadow-[8px_0_0_0_#c62121]",
     orange: "shadow-[8px_0_0_0_#ff9800]",
   };

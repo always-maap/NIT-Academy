d792b46f9faf14fb2e14007f63cb1e884ba46e0d
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-06-09 10:13:31 +0330

    feat: unavailable courses ui
    * feat: unavailable courses colored as white
    
    * fix: fixed scroll overflow by shortening page height

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-8">
           {courseList.map((category) => (
             <div key={category.title}>
-              <h2 className="text-2xl font-extrabold my-6">{category.title}</h2>
-              <ScrollArea className="lg:h-[80vh] rounded">
+              <h2 className="my-6 text-2xl font-extrabold">{category.title}</h2>
+              <ScrollArea className="lg:h-[78vh] rounded">
                 {category.subjects.map((subject) => (
                   <Link key={subject.title} href={subject.slug}>
-                    <Item variant={category.variant as any}>
+                    <Item variant={category.variant as any}  available={subject.slug !== ""}>
                       {subject.title}
                     </Item>
                   </Link>

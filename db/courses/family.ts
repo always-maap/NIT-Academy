9e09c07df2fe28ff4e9fd3b403859aa2dedc939c
tur1ng <mohammadali.ap.2000@gmail.com>
2023-06-15 20:28:57 +0330

    feat: add family and islamic-thoughts-2 summaries

--- db/courses/family.ts
+++ db/courses/family.ts
@@ -5,12 +5,19 @@ export const family: Course = {
     fa: "دانش خانواده و جمعیت",
     en: "family",
   },
+  callout: "طبق تجربه خلاصه درس به تنهایی برای کسب نمره کامل کافی نیست.",
   sources: [
     {
       id: "1",
-      title: "کتاب دانش خانواده و جمعیت",
+      title: "کتاب مرجع درس",
       type: "note",
       url: "https://nitacademy.s3.ir-thr-at1.arvanstorage.ir/family/family.pdf",
+    },
+    {
+      id: "2",
+      title: "خلاصه درس",
+      type: "note",
+      url: "https://nitacademy.s3.ir-thr-at1.arvanstorage.ir/family/summary.pdf",
     },
   ],
 };
--- db/courses/islamic-thoughts-2.ts
+++ db/courses/islamic-thoughts-2.ts
@@ -5,12 +5,19 @@ export const islamicThoughts2: Course = {
     fa: "اندیشه اسلامی ۲",
     en: "islamic-thoughts-2",
   },
+  callout: "طبق تجربه خلاصه درس به تنهایی برای کسب نمره کامل کافی نیست.",
   sources: [
     {
       id: "1",
       title: "کتاب اندیشه اسلامی ۲",
       type: "note",
       url: "https://nitacademy.s3.ir-thr-at1.arvanstorage.ir/islamic-thoughts-2/islamic-thoughts-2.pdf",
+    },
+    {
+      id: "2",
+      title: "خلاصه درس",
+      type: "note",
+      url: "https://nitacademy.s3.ir-thr-at1.arvanstorage.ir/islamic-thoughts-2/summary.pdf",
     },
   ],
 };

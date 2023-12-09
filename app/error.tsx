fdee70f67d44a1da952aa15150c31ca70fed230c
tur1ng <mohammadali.ap.2000@gmail.com>
2023-12-10 01:58:11 +0330

    fix: error page

---
+++ app/error.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect } from "react";
+
+export const runtime = "edge";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  );
+}

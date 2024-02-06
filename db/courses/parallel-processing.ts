06de6054178d19cd63ac18cd37e369f31cf6aaca
tur1ng <mohammadali.ap.2000@gmail.com>
2024-02-06 22:20:59 +0330

    fix: parallel processing note

--- db/courses/parallel-processing.ts
+++ db/courses/parallel-processing.ts
@@ -9,8 +9,8 @@ export const osLab: Course = {
     "این درس در مقطع کارشناسی ارشد برای اولین بار به صورت حضوری ارائه شده است ",
   sources: [
     {
-      id: "0",
-      title: "جلسه صفرم",
+      id: "1-note",
+      title: "جزوه کامل",
       type: "note",
       url: "https://nitacademy.s3.ir-thr-at1.arvanstorage.ir/parallel-processing/1-note.pdf",
     },
